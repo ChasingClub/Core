58ad8e17ebb225250b6ba16e9ed6c400254fbd2f
pinozenth <PInozen.Thailand@gmail.com>
2022-11-04 14:01:39 +0700

    added git ignore

--- Core-PVP/src/main/java/core/server/core/Utils/Utils.java
+++
@@ -1,9 +0,0 @@
-package core.server.core.Utils;
-
-import org.bukkit.ChatColor;
-
-import static core.server.core.Core.Plname;
-
-public class Utils {
-    public static String noperm = Plname+ ChatColor.RED+"You don't have permission to do that!";
-}
